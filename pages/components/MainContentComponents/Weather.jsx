6b62e7ac5d375bdcee0cfa0771e5fa9dacd144e1
Rafik Gajiyev <rafik.gadzhiyev.03@bk.ru>
2022-02-28 20:24:34 +0600

    issues: little issues was solved

--- pages/components/MainContentComponents/Weather.jsx
+++ pages/components/MainContentComponents/Weather.jsx
@@ -8,13 +8,17 @@ export default function Weather(props) {
 
     return (
         <li className={styles.current_day}>
-            <h2 className={styles.current_day_title}>{state.weekDays[new Date(weatherData.dt * 1000).getDay()]}</h2>
-            <i className={`${state.getWeatherIcon(weatherData.weather[0].id)} ${styles.current_day_icon}`}></i>
-            <div className={styles.current_day_temperature_container}>
-                <span className={styles.current_day_max_temperature}>Max: {parseInt(weatherData.temp.max - 273)}<sup>o</sup></span>
-                <span className={styles.current_day_min_temperature}>Min: {parseInt(weatherData.temp.min - 273)}<sup>o</sup></span>
-            </div>
-            <p className={styles.current_day_weather_description}>{weatherData.weather[0].description}</p>
+            {state &&
+                <>
+                    <h2 className={styles.current_day_title}>{state.weekDays[new Date(weatherData.dt * 1000).getDay()]}</h2>
+                    <i className={`${state.getWeatherIcon(weatherData.weather[0].id)} ${styles.current_day_icon}`}></i>
+                    <div className={styles.current_day_temperature_container}>
+                        <span className={styles.current_day_max_temperature}>Max: {parseInt(weatherData.temp.max - 273)}<sup>o</sup></span>
+                        <span className={styles.current_day_min_temperature}>Min: {parseInt(weatherData.temp.min - 273)}<sup>o</sup></span>
+                    </div>
+                    <p className={styles.current_day_weather_description}>{weatherData.weather[0].description}</p>
+                </>
+            }
         </li>
     )
 }
--- pages/components/MainContentComponents/WeatherMainDay.jsx
+++ pages/components/MainContentComponents/WeatherMainDay.jsx
@@ -10,33 +10,37 @@ export default function Today() {
         <div
             className={styles.today_weather_container}
         >
-            <p
-                className={styles.today_weather_country}
-            >{state.weatherData.city.name}, {state.weatherData.city.country}</p>
-            <p
-                className={styles.today_weather_temperature}
-            >{Math.round(todayWeather.main.temp - 273)}<sup>o</sup></p>
-            <i className={`${state.getWeatherIcon(todayWeather.weather[0].id)} ${styles.today_weather_icon}`}></i>
-            <p
-                className={styles.today_weather_description}
-            >{todayWeather.weather[0].description}</p>
-            <p
-                className={styles.today_weather_date}
-            >{state.weekDays[new Date(todayWeather.dt_txt).getDay()]} {todayWeather.dt_txt.slice(11)}</p>
-            <div
-                className={styles.today_weather_information}
-            >
-                <span
-                    className={styles.today_weather_information_text}
-                >Wind {todayWeather.wind.speed} m/s</span>
-                <span
-                    className={styles.today_weather_information_text}
-                >Pressure {todayWeather.main.pressure} hPa</span>
-                <span
-                    className={styles.today_weather_information_text}
-                >Humidity {todayWeather.main.humidity}%</span>
+            {state && todayWeather &&
+                <>
+                    <p
+                        className={styles.today_weather_country}
+                    >{state.weatherData.city.name}, {state.weatherData.city.country}</p>
+                    <p
+                        className={styles.today_weather_temperature}
+                    >{Math.round(todayWeather.main.temp - 273)}<sup>o</sup></p>
+                    <i className={`${state.getWeatherIcon(todayWeather.weather[0].id)} ${styles.today_weather_icon}`}></i>
+                    <p
+                        className={styles.today_weather_description}
+                    >{todayWeather.weather[0].description}</p>
+                    <p
+                        className={styles.today_weather_date}
+                    >{state.weekDays[new Date(todayWeather.dt_txt).getDay()]} {todayWeather.dt_txt.slice(11)}</p>
+                    <div
+                        className={styles.today_weather_information}
+                    >
+                        <span
+                            className={styles.today_weather_information_text}
+                        >Wind {todayWeather.wind.speed} m/s</span>
+                        <span
+                            className={styles.today_weather_information_text}
+                        >Pressure {todayWeather.main.pressure} hPa</span>
+                        <span
+                            className={styles.today_weather_information_text}
+                        >Humidity {todayWeather.main.humidity}%</span>
 
-            </div>
+                    </div>
+                </>
+            }
         </div>
     )
 }
